eec0b29eba2c5945ede4b5d069a6280e50a930e4
Zyrephus <wilsonchen315@gmail.com>
2025-06-13 16:15:56 -0400

    stuff

--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -5,4 +5,4 @@ def get_user_by_email(email: str):
     return response.data[0] if response.data else None
 
 def insert_user(email: str, password: str):
-    return supabase.table("User").insert({"email": email, "password": password}).execute()
+    return supabase.table("User").insert({"email": email, "password": password, "has_onboarded": False}).execute()
