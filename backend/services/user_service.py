b9347bdd8e6024037b312c3648f8c986722ebca5
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2025-06-13 16:35:43 -0400

    Modified insert user to work with nw schema

--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -1,8 +1,14 @@
 from supabase_client import supabase
+from datetime import datetime
 
 def get_user_by_email(email: str):
     response = supabase.table("User").select("*").eq("email", email).limit(1).execute()
     return response.data[0] if response.data else None
 
 def insert_user(email: str, password: str):
-    return supabase.table("User").insert({"email": email, "password": password, "has_onboarded": False}).execute()
+    return supabase.table("User").insert({
+        "email": email, 
+        "password": password, 
+        "has_onboarded": False, 
+        "last_login": datetime.now().isoformat()
+    }).execute()
